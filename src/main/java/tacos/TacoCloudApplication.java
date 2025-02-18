f543c3d8fdca47b1d601f38f37ac7b323ab2bcec
yoonyj <yoonyj2410@naver.com>
2025-02-18 22:57:27 +0900

    commit test

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication // 스프링 부트 어플리케이션임을 나타내는 어노테이션
 public class TacoCloudApplication {
-
+// test
 	public static void main(String[] args) {
 		SpringApplication.run(TacoCloudApplication.class, args);
 		// 애플리케이션을 실행한다.
