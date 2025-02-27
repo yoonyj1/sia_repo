487ac4e4f8b357688af01e79635102c3d55a0571
yoonyj <yoonyj2410@naver.com>
2025-02-27 23:37:29 +0900

    pom.xml 의존성 추가, p65

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -6,9 +6,12 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
@@ -48,6 +51,18 @@ public class DesignTacoController {
 	
 	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
 		return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
+	}
+	
+	@PostMapping
+	public String processDesign(@Valid Taco design, Errors errors) { // Valid annotaion은 Taco 객체의 validation과 매핑되어 유효성 검사 실시 후 에러 여부 확인
+		if(errors.hasErrors()) { // 에러가 있을 경우 상세 내역이 Errors 객체에 저장되어  processDesign으로 전달됨
+			return "design"; // 에러있을 경우 /design으로 다시 복귀
+		}
+		
+		// 해당 메소드에서 타코 디자인(/design에서 선택하고 넘어온 식자재 내역)을 저장
+		log.info("Processing Design: " + design);
+		
+		return "redirect:/orders/current";
 	}
 	
 }
